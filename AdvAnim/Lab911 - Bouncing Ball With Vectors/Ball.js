d14b87f37172e7f87d4c945fc66b688eb01ae73f
Peter Klopp <795294@seq.org>
2019-09-13 10:25:51 -0700

    Finihsed Ball Constructor

--- AdvAnim/Lab911 - Bouncing Ball With Vectors/Ball.js
+++ AdvAnim/Lab911 - Bouncing Ball With Vectors/Ball.js
@@ -3,33 +3,48 @@ function Ball(x, y, rad, vx, vy, ax, ay){
   this.vel = new JSVector(vx,vy);
   this.acc = new JSVector(ax,ay);
   this.radius = rad;
+  
 
   this.render = function() {
 
-    context.clearRect(0,0, canvas.width, canvas.height);
 
-    context.strokeStyle = 'red';
-    context.fillStyle = 'red';
+
+    context.strokeStyle = 'rgb(255,0,0)';
+    context.fillStyle = 'rgb(255,0,0)';
     context.beginPath();
 
     context.arc(this.loc.x, this.loc.y, this.radius, 0, Math.PI*2, true);
     context.stroke();
     context.fill();
 
+
   }
 
   this.update = function() {
 
-    window.requestAnimationFrame(update);
+
 
     this.loc.add(this.vel);
-    this.acc.add(this.acc);
+    this.vel.add(this.acc);
 
   }
 
   this.run = function() {
+
     this.update();
     this.render();
+    this.checkEdges();
+
+  }
+
+  this.checkEdges = function(){
+    if(this.loc.x + this.radius > canvas.width || this.loc.x - this.radius < 0){
+      this.vel.x = -this.vel.x;
+    }
+
+    if(this.loc.y + this.radius > canvas.height || this.loc.y - this.radius < 0){
+      this.vel.y = -this.vel.y;
+    }
   }
 
 
--- AdvAnim/Lab911 - Bouncing Ball With Vectors/sketch.js
+++ AdvAnim/Lab911 - Bouncing Ball With Vectors/sketch.js
@@ -3,14 +3,7 @@ addEventListener("load", setup);
 var canvas;
 var context;
 
-
-var loc = new JSVector(100,100);
-var vel = new JSVector(2,2);
-var acc = new JSVector(0,0.05);
-var radius = 30;
-
-let balls = [];
-
+let balls  = [];
 
 function setup(){
   canvas = document.getElementById("cnv");
@@ -23,46 +16,30 @@ function setup(){
   canvas.style.border = 'solid black 2px';
   canvas.style.backgroundColor = 'rgba(0,0,0,0.5)';
 
+  draw();
+
   loadBalls(50);
+
+
 
 }
 
 function draw(){
-  for(let i = 0; i < balls.length; i++){
+  requestAnimationFrame(draw);
 
-    requestAnimationFrame(draw);
+  context.clearRect(0,0, canvas.width, canvas.height);
 
-    context.clearRect(0,0, canvas.width, canvas.height);
+  for(let i = 0; i<balls.length;i++){
 
-    context.strokeStyle = 'red';
-    context.fillStyle = 'red';
-    context.beginPath();
-
-    context.arc(loc.x, loc.y, radius, 0, Math.PI*2, true);
-    context.stroke();
-    context.fill();
-
-    loc.add(vel);
-    vel.add(acc);
-
-    checkEdges();
-
+    balls[i].run();
   }
+
 
 }
 
-function checkEdges(){
-  if(loc.x + radius > canvas.width || loc.x - radius < 0){
-    vel.x = -vel.x;
-  }
-
-  if(loc.y + radius > canvas.height || loc.y - radius < 0){
-    vel.y = -vel.y;
-  }
-}
 
 function loadBalls(n){
-  for(let i = 0; i < n; i++){
-    draw();
+  for(let i = 0; i<n; i++){
+    balls.push(new Ball(Math.random()*800,Math.random()*800,30,Math.random()*4,2,0,0.05));
   }
 }
