dbd24cf9ba8a785fa35cfd7b990eab01f54dc821
Peter Klopp <795294@seq.org>
2019-09-23 10:29:42 -0700

    Attraction and Repulsion

---
+++ AdvAnim/Lab 919 - Attraction and Repulsion/Ball.js
@@ -0,0 +1,114 @@
+function Ball(x, y, rad, vx, vy, ax, ay, clr){
+  this.loc = new JSVector(x,y);
+  this.vel = new JSVector(vx,vy);
+  this.acc = new JSVector(ax,ay);
+  this.repulsion =
+  this.radius = rad;
+  this.mass = this.radius*10;
+  this.color = clr;
+
+
+  this.render = function() {
+
+    if(this === repulsionBall){
+      context.strokeStyle = this.color;
+      context.fillStyle = this.color;
+      context.beginPath();
+
+      context.arc(this.loc.x, this.loc.y, this.radius, 0, Math.PI*2, true);
+      context.stroke();
+      context.fill();
+
+    } else if(this === attractionBall){
+      context.strokeStyle = this.color;
+      context.fillStyle = this.color;
+      context.beginPath();
+
+      context.arc(this.loc.x, this.loc.y, this.radius, 0, Math.PI*2, true);
+      context.stroke();
+      context.fill();
+
+    } else {
+      context.strokeStyle = this.color;
+      context.fillStyle = this.color;
+      context.beginPath();
+
+      context.arc(this.loc.x, this.loc.y, this.radius, 0, Math.PI*2, true);
+      context.stroke();
+      context.fill();
+    }
+
+
+  }
+
+  this.update = function() {
+
+    this.loc.add(this.vel);
+    this.vel.add(this.acc);
+
+    this.vel.limit(4);
+
+  }
+
+  this.run = function() {
+
+    this.update();
+    this.render();
+    this.checkEdges();
+
+  }
+
+  this.checkEdges = function(){
+    
+      if(this.loc.x + this.radius > canvas.width || this.loc.x - this.radius < 0){
+        this.vel.x = -this.vel.x;
+      }
+
+      if(this.loc.y + this.radius > canvas.height || this.loc.y - this.radius < 0){
+        this.vel.y = -this.vel.y;
+      }
+  }
+
+  this.connect = function(v2){
+    var d = this.loc.distance(v2.loc);
+
+    if(d<100){
+
+      context.lineWidth = 2;
+      context.strokeStyle = 'rgb(0,100,100)';
+      context.moveTo(this.loc.x, this.loc.y);
+      context.lineTo(v2.loc.x, v2.loc.y);
+      context.stroke();
+
+    }
+  }
+
+  this.attract = function(v2){
+    var d = this.loc.distance(v2.loc);
+
+    if(d<200){
+
+      var attractionForce = JSVector.subGetNew(v2.loc, this.loc);
+      attractionForce.normalize();
+      attractionForce.multiply(0.5);
+      this.vel.add(attractionForce);
+
+    }
+
+
+  }
+
+  this.repel = function(v2){
+    var d = this.loc.distance(v2.loc);
+
+    if(d<500){
+
+      var repulsuionForce = JSVector.addGetNew(v2.loc, this.loc);
+      repulsuionForce.normalize();
+      repulsuionForce.multiply(0.5);
+      this.vel.add(repulsuionForce);
+
+    }
+
+  }
+}
---
+++ AdvAnim/Lab 919 - Attraction and Repulsion/sketch.js
@@ -0,0 +1,68 @@
+addEventListener("load", setup);
+
+var canvas;
+var context;
+
+let balls  = [];
+
+var repulsionBall;
+var attractionBall;
+
+function setup(){
+  canvas = document.getElementById("cnv");
+
+  canvas.width = 800;
+  canvas.height = 800;
+
+  context = canvas.getContext("2d");
+
+  canvas.style.border = 'solid black 2px';
+  canvas.style.backgroundColor = 'rgba(0,0,0,0.5)';
+
+  loadBalls(20);
+
+  repulsionBall = new Ball(Math.random()*(730-70)+70,Math.random()*(730-70)+70,70,(Math.random()*2)-1,(Math.random()*4)-2,0,0, 'rgb(0,255,0)' );
+  attractionBall = new Ball(Math.random()*(730-70)+70,Math.random()*(730-70)+70,70,(Math.random()*2)-1,(Math.random()*4)-2,0,0, 'rgb(255,0,0)');
+
+  draw();
+
+}
+
+function draw(){
+  requestAnimationFrame(draw);
+
+  context.clearRect(0,0, canvas.width, canvas.height);
+
+  repulsionBall.run();
+
+  attractionBall.run();
+
+
+  for(let i = 0; i<balls.length;i++){
+    balls[i].run();
+
+    balls[i].attract(attractionBall);
+
+    balls[i].repel(repulsionBall);
+
+    for(let j = 0; j < balls.length; j++){
+      if(balls[i] != balls[j]){
+        balls[i].connect(balls[j]);
+      }
+    }
+
+
+  }
+
+
+}
+
+
+function loadBalls(n){
+  for(let i = 0; i<n; i++){
+
+
+    balls.push(new Ball(Math.random()*(770-30)+30,Math.random()*(770-30)+30,Math.random()*30,(Math.random()*4)-2,(Math.random()*4)-2,0,0, 'rgb(' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ')'));
+
+  }
+}
