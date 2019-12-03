0405b716141049ef25a7e68d73d2bc667f1f1763
Peter Klopp <peterklopp02@gmail.com>
2019-12-03 11:45:59 -0800

    Attempt to move canvas around in world

--- AdvAnim/Labs/Lab1203 - MiniMap/sketch.js
+++ AdvAnim/Labs/Lab1203 - MiniMap/sketch.js
@@ -12,6 +12,9 @@ var canvasHeight = 600;
 var worldWidth = 4000;
 var worldHeight = 3000;
 
+var canvasX = -1600;   // where the origin is
+var canvasY = -1200;
+
 
 function setup(){
   canvas = document.getElementById("cnv");
@@ -36,37 +39,29 @@ function draw(){
 
   context.clearRect(0,0, canvas.width, canvas.height);
 
+
+
   for(let i = 0; i < snakes.length; i++){
 
     snakes[i].run();
   }
+
+    context.setTransform(1,0,0,1,0,0);
+
+    context.translate(-canvasX, -canvasY);
 }
 
 function loadSnakes(numSnakes){
   for(let i = 0; i<numSnakes; i++){
-    snakes.push(new Snake(Math.random()*canvas.width, Math.random()*canvas.width, (Math.random()*10)-5, (Math.random()*10)-5, 30));
+    snakes.push(new Snake(Math.random()*canvas.width, Math.random()*canvas.height, (Math.random()*10)-5, (Math.random()*10)-5, 30));
 
     snakes[i].loadSegments(10);
   }
 }
 
-// function cloneCanvas(oldCanvas) {
-//
-//     //create a new canvas
-//     var newCanvas = document.createElement('canvas');
-//     var context = newCanvas.getContext('2d');
-//
-//     //set dimensions
-//     newCanvas.width = oldCanvas.width;
-//     newCanvas.height = oldCanvas.height;
-//
-//     //apply the old canvas to the new one
-//     context.drawImage(oldCanvas, 0, 0);
-//
-//     //return the new canvas
-//     return newCanvas;
-// }
+function moveCanvas(keyPressed){
 
-function moveCanvas(e){
-  console.log(e);
+  if(keyPressed.code = "ArrowRight"){
+    canvasX -= 1;
+  }
 }
