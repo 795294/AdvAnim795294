087937a33216d2f5732dbbedb887c8e6ebbd995e
795294 <795294@seq.org>
2020-01-31 09:50:14 -0800

    bdfdgf

---
+++ Semester 2/Lab122 - Matter.js canvas rendering context/pyramid.js
@@ -0,0 +1,27 @@
+function Pyramid(xPos, yPos, width, height, rows, cols){
+  this.xPos = xPos;
+  this.yPos = yPos;
+  this.width = width;
+  this.height = height;
+  this.rows = rows;
+  this.cols = cols;
+
+  this.pyramid = Composites.pyramid(this.xPos, this.yPos, this.rows, this.cols, 0, 0, function(x, y) {
+    var n = new Rectangle(x, y, this.width, this.height);
+    return n.newRect;
+  });
+
+
+  this.run = function(){
+    var pArray = this.pyramid.bodies;
+    for(let i = 0; i < pArray.length; i++){
+      context.save();
+      context.translate(100, 100);
+      var direction = pArray[i].angle;
+      context.rotate(direction);
+      context.rect(-.5*20, -.5*40, 20, 40);
+      context.restore();
+    }
+  }
+
+}
